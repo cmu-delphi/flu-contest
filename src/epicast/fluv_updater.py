9fcd4794cb7fcaf484c4f95047538965f53541cc
undefx <dfarrow0@gmail.com>
2017-09-11 18:24:08 -0700

    don't update during the offseason

--- src/epicast/fluv_updater.py
+++ src/epicast/fluv_updater.py
@@ -3,15 +3,20 @@
 === Purpose ===
 ===============
 
+*** WARNING: This will trigger scoring and emails! ***
+
 Performs database updates for Epicast-FLUV:
   `history` update: import wILI from Epidata API
   `round` update: increments the deadline by a week
+
 
 
 =================
 === Changelog ===
 =================
 
+2017-09-11
+  * don't update during the offseason
 2016-12-14
   + use secrets
 2015-12-04
@@ -74,59 +79,71 @@ def check(res):
     raise Exception('API failure (%d|%s)' % (res['result'], res['message']))
   return res['epidata']
 
-# DB stuff
-u, p = secrets.db.epi
-cnx = mysql.connector.connect(user=u, password=p, database='epicast2')
-cur = cnx.cursor()
 
-# Do work
-if args.action == 'history':
-  print('Updating ec_fluv_history')
-  epiweek_new = check(Epidata.fluview(['nat'], Epidata.range(201501, 202001)))[-1]['epiweek']
-  rows1 = get_rows(cnx)
-  print('rows before: %d' % (rows1))
-  execute_sql(cur, 'SELECT coalesce(max(`epiweek`), 0) FROM ec_fluv_history', True)
-  for (epiweek_old, ) in cur:
-    pass
-  print('Old epiweek: %d | New epiweek: %d' % (epiweek_old, epiweek_new))
-  if epiweek_old == epiweek_new:
-    print('No new data - nothing to do')
-  elif epiweek_old < epiweek_new:
-    print('New data available!')
-    # Generate the new data table and do the swap
-    execute_sql(cur, 'UPDATE ec_fluv_round SET `data_epiweek` = %d' % epiweek_new)
-    execute_sql(cur, 'TRUNCATE TABLE ec_fluv_history_stage')
-    for (region_id, region_label) in zip([i for i in range(1, 12)], ['nat'] + ['hhs%d' % r for r in range(1, 11)]):
-      print(' Updating %s...' % region_label)
-      if args.test: continue
-      for row in check(Epidata.fluview(region_label, Epidata.range(199740, 202001))):
-        cur.execute('INSERT INTO ec_fluv_history_stage (`region_id`, `epiweek`, `wili`) VALUES (%s, %s, %s)', (region_id, row['epiweek'], row['wili']))
-    execute_sql(cur, 'ALTER TABLE ec_fluv_history RENAME TO ec_fluv_history_temp')
-    execute_sql(cur, 'ALTER TABLE ec_fluv_history_stage RENAME TO ec_fluv_history')
-    execute_sql(cur, 'ALTER TABLE ec_fluv_history_temp RENAME TO ec_fluv_history_stage')
-    execute_sql(cur, 'TRUNCATE TABLE ec_fluv_history_stage')
-    # Queue an email to myself
-    execute_sql(cur, "INSERT INTO automation.email_queue (`from`, `to`, `subject`, `body`) VALUES ('%s', '%s', '[DELPHI] New FluView Data', 'Another week of FluView data is now available - Happy forecasting!')" % (secrets.flucontest.email_epicast, secrets.flucontest.email_maintainer))
-    execute_sql(cur, 'CALL automation.RunStep(2)')
-    # Queue the score updater
-    execute_sql(cur, 'CALL automation.RunStep(8)')
-    # Queue the notification emails
-    execute_sql(cur, 'CALL automation.RunStep(9)')
-  else:
-    raise Exception('The new data is older than the old data?!')
-  rows2 = get_rows(cnx)
-  print('rows after: %d (added %d)' % (rows2, rows2 - rows1))
-elif args.action == 'round':
-  print('Updating ec_fluv_round')
-  execute_sql(cur, 'SELECT `round_epiweek`, `deadline` FROM ec_fluv_round', True)
-  for (epiweek, deadline) in cur:
-    pass
-  print('Last round was %d, due %s' % (epiweek, deadline))
-  epiweek = add_epiweeks(epiweek, 1)
-  print('Next round is %d' % (epiweek))
-  execute_sql(cur, 'UPDATE ec_fluv_round SET `round_epiweek` = %d, `deadline` = date_add(`deadline`, INTERVAL 1 WEEK)' % (epiweek))
+def main():
+  # DB stuff
+  u, p = secrets.db.epi
+  cnx = mysql.connector.connect(user=u, password=p, database='epicast2')
+  cur = cnx.cursor()
 
-# Cleanup
-cnx.commit()
-cur.close()
-cnx.close()
+  # Do work
+  if args.action == 'history':
+    print('Updating ec_fluv_history')
+    epiweek_new = check(Epidata.fluview(['nat'], Epidata.range(201501, 203001)))[-1]['epiweek']
+    rows1 = get_rows(cnx)
+    print('rows before: %d' % (rows1))
+    execute_sql(cur, 'SELECT coalesce(max(`epiweek`), 0) FROM ec_fluv_history', True)
+    for (epiweek_old, ) in cur:
+      pass
+    print('Old epiweek: %d | New epiweek: %d' % (epiweek_old, epiweek_new))
+    if epiweek_old == epiweek_new:
+      print('No new data - nothing to do')
+    elif epiweek_old < epiweek_new:
+      print('New data available!')
+      # Check to see if we are in the offseason
+      execute_sql(cur, 'SELECT `first_round_epiweek`, `last_round_epiweek` FROM ec_fluv_season', True)
+      for (ew1, ew2) in cur:
+        if not (ew1 <= epiweek_new <= ew2):
+          print('new data is outside of current season [%d-%d]' % (ew1, ew2))
+          return
+      # Generate the new data table and do the swap
+      execute_sql(cur, 'UPDATE ec_fluv_round SET `data_epiweek` = %d' % epiweek_new)
+      execute_sql(cur, 'TRUNCATE TABLE ec_fluv_history_stage')
+      for (region_id, region_label) in zip([i for i in range(1, 12)], ['nat'] + ['hhs%d' % r for r in range(1, 11)]):
+        print(' Updating %s...' % region_label)
+        if args.test: continue
+        for row in check(Epidata.fluview(region_label, Epidata.range(199740, 203001))):
+          cur.execute('INSERT INTO ec_fluv_history_stage (`region_id`, `epiweek`, `wili`) VALUES (%s, %s, %s)', (region_id, row['epiweek'], row['wili']))
+      execute_sql(cur, 'ALTER TABLE ec_fluv_history RENAME TO ec_fluv_history_temp')
+      execute_sql(cur, 'ALTER TABLE ec_fluv_history_stage RENAME TO ec_fluv_history')
+      execute_sql(cur, 'ALTER TABLE ec_fluv_history_temp RENAME TO ec_fluv_history_stage')
+      execute_sql(cur, 'TRUNCATE TABLE ec_fluv_history_stage')
+      # Queue an email to myself
+      execute_sql(cur, "INSERT INTO automation.email_queue (`from`, `to`, `subject`, `body`) VALUES ('%s', '%s', '[DELPHI] New FluView Data', 'Another week of FluView data is now available - Happy forecasting!')" % (secrets.flucontest.email_epicast, secrets.flucontest.email_maintainer))
+      execute_sql(cur, 'CALL automation.RunStep(2)')
+      # Queue the score updater
+      execute_sql(cur, 'CALL automation.RunStep(8)')
+      # Queue the notification emails
+      execute_sql(cur, 'CALL automation.RunStep(9)')
+    else:
+      raise Exception('The new data is older than the old data?!')
+    rows2 = get_rows(cnx)
+    print('rows after: %d (added %d)' % (rows2, rows2 - rows1))
+  elif args.action == 'round':
+    print('Updating ec_fluv_round')
+    execute_sql(cur, 'SELECT `round_epiweek`, `deadline` FROM ec_fluv_round', True)
+    for (epiweek, deadline) in cur:
+      pass
+    print('Last round was %d, due %s' % (epiweek, deadline))
+    epiweek = add_epiweeks(epiweek, 1)
+    print('Next round is %d' % (epiweek))
+    execute_sql(cur, 'UPDATE ec_fluv_round SET `round_epiweek` = %d, `deadline` = date_add(`deadline`, INTERVAL 1 WEEK)' % (epiweek))
+
+  # Cleanup
+  cnx.commit()
+  cur.close()
+  cnx.close()
+
+
+if __name__ == '__main__':
+  main()
