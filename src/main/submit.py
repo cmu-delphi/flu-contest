f350a1fbffbcade457665fed82c3df5da19485c7
ChrisSheng97 <chrissheng97@gmail.com>
2017-11-10 12:05:13 -0500

    add tx and or to states

--- src/main/submit.py
+++ src/main/submit.py
@@ -89,7 +89,7 @@ import delphi.utils.epiweek as flu
 
 regions = ['nat'] + ['hhs%d' % i for i in range(1, 11)]
 af_locations = regions
-ec_locations = regions + ['dc', 'ga', 'pa']
+ec_locations = regions + ['dc', 'ga', 'pa', 'tx', 'or']
 
 
 def get_expected_issue():
