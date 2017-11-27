3a3c1f55668ed6e961bc39ba21d7b772c87c07c0
ChrisSheng97 <chrissheng97@gmail.com>
2017-11-27 13:15:11 -0500

    change ddl for Thanksgiving

--- src/epicast/fluv_emailer.py
+++ src/epicast/fluv_emailer.py
@@ -198,29 +198,25 @@ You can find the leaderboards at http://epicast.org/scores.php
 # NOTIFICATIONS                                                                #
 ################################################################################
       'notifications': {
-        'subject': 'New Data Available (Note: Deadline Moved to Monday 10AM)',
+        'subject': 'New Data Available (Note: Deadline This Week is Wednesday 10AM)',
         'text': '''
 Dear %s,
-The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! Note that due to a change in the forecast submission schedule, we are now asking you to submit your forecasts by 10:00 AM (ET) this coming %s. 
-This new deadline will hold for the rest of the season. Thank you so much for your support and cooperation!
-Accurate forecasting of flu’s spread in different regions is important for timely public alerts, education campaigns, vaccination campaigns, staffing decisions and resource allocations.  For several years now, CDC has been supporting the development of flu forecasting technology via its annual “Predict the Flu” challenge. Epicast was the most accurate forecasting systems in CDC's “Predict the Flu” challenge for the 2016-17 season, in a crowded field of 28 submissions. It was originally developed by David Farrow as part of his PhD thesis.
+Apologize for the wrong deadline in the previous email. Due to the Thanksgiving holiday, this week we are asking you to submit your forecasts by 10:00 AM (ET) this coming Wednesday.
+Thank you so much for your support and cooperation!
 To login and submit your forecasts, visit http://epicast.org and enter your User ID: %s
 %s
 Thank you again for your participation, and good luck on your forecasts!
 Happy Forecasting!
 -The DELPHI Team
-        '''%(u[1], deadline_day, u[0], score_text),
+        '''%(u[1], u[0], score_text),
         'html': '''
 
 
 <p>
   Dear %s,
 </p><p>
-  The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! Note that due to a change in the forecast submission schedule, we are now asking you to submit your forecasts by <b>10:00 AM (ET)</b> this coming %s. 
-  This new deadline will hold for the rest of the season. Thank you so much for your support and cooperation!
-</p><p>
-  Accurate forecasting of flu’s spread in different regions is important for timely public alerts, education campaigns, vaccination campaigns, staffing decisions and resource allocations. For several years now, CDC has been supporting the development of flu forecasting technology via its annual “Predict the Flu” challenge. 
-  Epicast was the most accurate forecasting systems in CDC's “Predict the Flu” challenge for the 2016-17 season, in a crowded field of 28 submissions. It was originally developed by David Farrow as part of his PhD thesis.
+  Apologize for the wrong deadline in the previous email. Due to the Thanksgiving holiday, this week we are asking you to submit your forecasts by <b>10:00 AM (ET) this coming Wednesday </b>.
+Thank you so much for your support and cooperation!
 </p><p>
   To login and submit your forecasts, click <a href="http://epicast.org/launch.php?user=%s">here</a> or visit http://epicast.org and enter your User ID: %s
 </p>%s<p>
@@ -230,18 +226,18 @@ Happy Forecasting!
 <br />
   -The DELPHI Team
 </p>
-        '''%(u[1], deadline_day, u[0], u[0], score_html),
+        '''%(u[1], u[0], u[0], score_html),
       },
 ################################################################################
 # REMINDERS                                                                    #
 ################################################################################
       'reminders': {
-        'subject': 'Forecasts Needed (Reminder: Deadline Moved to Monday 10AM)',
+        'subject': 'Forecasts Needed (Reminder: Deadline This Week is Wednesday 10AM)',
         'text': '''
 Dear %s,
 
 
-This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on %s. Note that due to a change in the forecast submission schedule, the official deadline now is 10:00 AM Monday, and this will hold for the rest of the season. 
+This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on Wednesday, due to the Thanksgiving holiday. 
 Thank you so much for your support and cooperation!
 
 To login and submit your forecasts, visit http://epicast.org and enter your User ID: %s
@@ -250,13 +246,13 @@ To login and submit your forecasts, visit http://epicast.org and enter your User
 Happy Forecasting!
 
 -The DELPHI Team
-        '''%(u[1], deadline_day, u[0]),
+        '''%(u[1], u[0]),
         'html': '''
 <p>
   Dear %s,
 </p><p>
-  This is just a friendly reminder that your flu forecasts are due by <b>10:00AM (ET)</b> on %s. Note that due to a change in the forecast submission schedule, we are now moving our deadline to 10:00 AM Monday, and this will hold for the rest of the season. 
-  Thank you so much for your support and cooperation!
+  This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on Wednesday, due to the Thanksgiving holiday. 
+Thank you so much for your support and cooperation!
 </p><p>
   To login and submit your forecasts, click <a href="http://epicast.org/launch.php?user=%s">here</a> or visit http://epicast.org and enter your User ID: %s
 </p><p>
@@ -264,7 +260,7 @@ Happy Forecasting!
   <br />
   -The DELPHI Team
 </p>
-        '''%(u[1], deadline_day, u[0], u[0]),
+        '''%(u[1], u[0], u[0]),
       },
 ################################################################################
     }
