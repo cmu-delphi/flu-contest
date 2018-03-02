d39d6921092deb1d1f18412894fe0aaec272db69
ChrisSheng97 <chrissheng97@gmail.com>
2018-03-02 12:27:49 -0500

    update email

--- src/epicast/fluv_emailer.py
+++ src/epicast/fluv_emailer.py
@@ -198,12 +198,15 @@ You can find the leaderboards at http://epicast.org/scores.php
 # NOTIFICATIONS                                                                #
 ################################################################################
       'notifications': {
-        'subject': 'New Data Available (Deadline: Monday 10AM)',
+        'subject': '[Epicast] New Data Available (NEW: 90% confidence intervals)',
         'text': '''
+        
+<b>NEW THIS WEEK</b>: you will notice grey vertical bars on the flu activity levels in the last few weeks of the current season.  This is because these values are subject to future revision.  The vertical bars represent 90% confidence intervals for where the final values will end up being.
+
 Dear %s,
-The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! We are asking you to please submit your forecasts by 10:00 AM (ET) this coming %s.
+The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! We are asking you to please submit your forecasts by <b>10:00 AM (ET)</b> this coming %s.
 Thank you so much for your support and cooperation!
-To adjust the maximum forecast value for forecast, click the up/down button on the top left of forecast page (located at the top of y-axis). 
+ 
 Accurate forecasting of flu’s spread in different regions is important for timely public alerts, education campaigns, vaccination campaigns, staffing decisions and resource allocations.  For several years now, CDC has been supporting the development of flu forecasting technology via its annual “Predict the Flu” challenge. Epicast was the most accurate forecasting systems in CDC's “Predict the Flu” challenge for the 2016-17 season, in a crowded field of 28 submissions. It was originally developed by David Farrow as part of his PhD thesis.
 To login and submit your forecasts, visit http://epicast.org and enter your User ID: %s
 %s
@@ -215,12 +218,12 @@ Happy Forecasting!
 
 
 <p>
+<b>NEW THIS WEEK</b>: you will notice grey vertical bars on the flu activity levels in the last few weeks of the current season.  This is because these values are subject to future revision.  The vertical bars represent 90% confidence intervals for where the final values will end up being.
+</p><p>
   Dear %s,
 </p><p>
   The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! We are asking you to please submit your forecasts by <b>10:00 AM (ET)</b> this coming Monday.
   Thank you so much for your support and cooperation!
-</p><p>
-  To adjust the maximum wILI value for forecast, click the <b>up/down button</b> on the <b>top left</b> of the forecast page (located at the top of the y-axis).
 </p><p>
   Accurate forecasting of flu’s spread in different regions is important for timely public alerts, education campaigns, vaccination campaigns, staffing decisions and resource allocations. For several years now, CDC has been supporting the development of flu forecasting technology via its annual “Predict the Flu” challenge.
   Epicast was the most accurate forecasting systems in CDC's “Predict the Flu” challenge for the 2016-17 season, in a crowded field of 28 submissions. It was originally developed by David Farrow as part of his PhD thesis.
