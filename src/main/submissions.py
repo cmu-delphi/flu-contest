13fba844627c966beabf108bf90d0ecbb505c181
ChrisSheng97 <chrissheng97@gmail.com>
2018-10-25 13:58:55 -0400

    add complete list of regions for test run

--- src/main/submissions.py
+++ src/main/submissions.py
@@ -88,7 +88,8 @@ if __name__ == '__main__':
   print(' - Assuming last published wILI on %d' % epiweek)
   print(' - Limited locations')
 
-  sub = Submissions(['nat', 'hhs5', 'pa'], 1000)
+  regions = ['nat', 'hhs1','hhs2','hhs3','hhs4','hhs5','hhs6','hhs7','hhs8','hhs9','hhs10', 'pa', 'ga', 'tx', 'or', 'dc']
+  sub = Submissions(regions, 10000)
   ec, af = None, None
   ec = sub.run_epicast(epiweek, 0.001, 0.001)
   #af = sub.run_archefilter(epiweek, 0.002, 0.002, num_samples=1000)
