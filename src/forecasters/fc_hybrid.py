fb4301aa40ac69ddfc9c80334883f9f37dbd9ca2
ChrisSheng97 <chrissheng97@gmail.com>
2018-10-25 19:04:26 -0400

    debug snapshot

--- src/forecasters/fc_hybrid.py
+++ src/forecasters/fc_hybrid.py
@@ -43,13 +43,9 @@ class Hybrid(Forecaster):
     self.future.close()
 
   def _forecast(self, region, epiweek):
-#     print('inside hybrid._forecast, region, epiweek:', region, epiweek)
-#     P = self.past._forecast(region, epiweek)
-#     F = self.future._forecast(region, epiweek)
-    P = self.past._forecast(region, 201841)
-    F = self.future._forecast(region, 201841)
-    print('temp F length:', len(F))
-    F = self.future._forecast(region, 201842)
+    print('inside hybrid._forecast, region, epiweek:', region, epiweek)
+    P = self.past._forecast(region, epiweek)
+    F = self.future._forecast(region, epiweek)
     print('inside hybrid._forecast, len P, len F', len(P), len(F))
     i = flu.delta_epiweeks(flu.join_epiweek(self.test_season, 40), epiweek)
     curves = []
