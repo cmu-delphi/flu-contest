e96068417d5c1ceb5029b75a350a4135b84db598
ChrisSheng97 <chrissheng97@gmail.com>
2019-01-05 22:16:11 +0800

    update email

--- src/epicast/fluv_emailer.py
+++ src/epicast/fluv_emailer.py
@@ -198,11 +198,11 @@ You can find the leaderboards at http://epicast.org/scores.php
 # NOTIFICATIONS                                                                #
 ################################################################################
       'notifications': {
-        'subject': 'New Data Available (Deadline: Wednesday 10 AM)',
+        'subject': 'New Data Available (Deadline: Monday 10 AM)',
         'text': '''
         
 Dear %s,
-The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! Due to New Year's Day, we are asking you to please submit your forecasts by <b>10:00 AM (ET)</b> this coming Wednesday.
+The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! We are asking you to please submit your forecasts by <b>10:00 AM (ET)</b> this coming Monday.
 Thank you so much for your support and cooperation!
  
 To login and submit your forecasts, visit http://epicast.org and enter your User ID: %s
@@ -217,7 +217,7 @@ Happy Forecasting!
 <p>
   Dear %s,
 </p><p>
-  The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! Due to New Year's Day, we are asking you to please submit your forecasts by <b>10:00 AM (ET)</b> this coming Wednesday.
+  The CDC has released another week of flu surveillance data. A new round of flu forecasting is now underway, and we need your forecasts! We are asking you to please submit your forecasts by <b>10:00 AM (ET)</b> this coming Monday.
   Thank you so much for your support and cooperation!
 </p><p>
   To login and submit your forecasts, click <a href="http://epicast.org/launch.php?user=%s">here</a> or visit http://epicast.org and enter your User ID: %s
@@ -236,12 +236,12 @@ Happy Forecasting!
 # REMINDERS                                                                    #
 ################################################################################
       'reminders': {
-        'subject': 'Forecasts Needed (Deadline: Wednesday 10AM)',
+        'subject': 'Forecasts Needed (Deadline: Monday 10AM)',
         'text': '''
 Dear %s,
 
 
-This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on Wednesday. 
+This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on Monday. 
 Thank you so much for your support and cooperation!
 
 To login and submit your forecasts, visit http://epicast.org and enter your User ID: %s.
@@ -255,7 +255,7 @@ Happy Forecasting!
 <p>
   Dear %s,
 </p><p>
-  This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on Wednesday. 
+  This is just a friendly reminder that your flu forecasts are due by 10:00AM (ET) on Monday. 
 Thank you so much for your support and cooperation!
 </p><p>
   To login and submit your forecasts, visit http://epicast.org and enter your User ID: %s.
