ee63813448b4bb2168af35266f1ebd90c7becbdb
ChrisSheng97 <chrissheng97@gmail.com>
2019-02-25 22:49:16 -0500

    Create forecast_io_mturk.py

---
+++ src/utils/forecast_io_mturk.py
@@ -0,0 +1,275 @@
+"""
+Forecast serialization and deserialization.
+
+Specific to the 2017-2018 flu season.
+"""
+
+# standard library
+import csv
+import datetime
+import json
+import os
+import re
+
+# first party
+from delphi.flu_contest.utils.forecast import Forecast
+from delphi.flu_contest.utils.forecast_meta import Locations, Targets, Types
+from delphi.utils.epidate import EpiDate
+import delphi.utils.epiweek as Epiweek
+
+
+class ForecastIO:
+
+  _version = 3
+
+  @staticmethod
+  def int_or_float(x):
+    # cast as int, but only if it doesn't change the value
+    xf = float(x)
+    xi = int(xf)
+    return xi if xi == xf else xf
+
+  @staticmethod
+  def get_week_index(week):
+    if not (40 <= week <= 52) and not (1 <= week <= 20):
+      raise Exception('invalid week: %d' % week)
+    return week - 40 if week >= 40 else week + 12
+
+  @staticmethod
+  def get_index_week(index):
+    if not (0 <= index <= 32):
+      raise Exception('invalid index: %d' % index)
+    return index + 40 if index <= 12 else index - 12
+
+  @staticmethod
+  def __load_row(forecast, location, target, type_, start, value):
+    # convert to delphi names
+    location = Locations.get_delphi_name(location)
+    target = Targets.get_delphi_name(target)
+    type_ = Types.get_delphi_name(type_)
+
+    # get the index, which is either a name or a number
+    if type_ == 'dist':
+      if start == 'none':
+        # no onset
+        index = 'none'
+      elif Targets.is_ili(target):
+        # ili bin index
+        index = round(float(start) * 10)
+      else:
+        # week number
+        index = ForecastIO.get_week_index(int(start))
+    else:
+      # point prediction
+      index = 'point'
+
+    # parse the value
+    value = ForecastIO.int_or_float(value)
+
+    # finally, set the value
+    fc = forecast.get_or_create_forecast(location)
+    fc.set_single_value(target, index, value)
+
+  @staticmethod
+  def extract_epiweek_and_team(filename):
+    """
+    Extract the submission epiweek (epiweek of most recently published report)
+    and the team name from the file name of a flu contest submission.
+
+    The return value is a tuple of:
+      1. the submission epiweek (e.g. 201751)
+      2. the team name (e.g. "delphi-epicast")
+    """
+
+    # this is the naming convention for 2017 flu contest submissions
+    pattern = re.compile('^EW(\\d{2})-(.*)-(\\d{4})-(\\d{2})-(\\d{2}).csv$')
+    match = pattern.match(os.path.basename(filename))
+    if match is None:
+      # only able to parse this specific naming convention
+      raise Exception()
+
+    week = int(match.group(1))
+    team = match.group(2)
+    year = int(match.group(3))
+    month = int(match.group(4))
+    day = int(match.group(5))
+    epiweek = EpiDate(year, month, day).get_ew()
+
+    # We know the week number, but the year has to be inferred from the
+    # submission date. Since the week of submission is never less than the week
+    # of the most recent report, we can step backwards from the week of
+    # submission until we find the expected week number. Ordinarily, this will
+    # take exactly two steps. For example, data collected on 2017w51 is
+    # reported on 2017w52, and our forecast is submitted on 2018w01; so we
+    # start with 2018w01 and step backwards until find the first week 51, which
+    # is 2017w51.
+    if not 1 <= week <= 53:
+      # prevent an infinite loop
+      raise Exception('invalid week number: %d' % week)
+    while Epiweek.split_epiweek(epiweek)[1] != week:
+      epiweek = Epiweek.add_epiweeks(epiweek, -1)
+
+    return epiweek, team
+
+  @staticmethod
+  def load_csv(filename):
+    timestamp = None
+    epiweek, team = ForecastIO.extract_epiweek_and_team(filename)
+
+    season_start = Epiweek.get_season(epiweek)[0]
+    season = Epiweek.split_epiweek(season_start)[0]
+
+    forecast = Forecast(season, timestamp, team, epiweek)
+
+    # the default column layout
+    # can be updated based on header row
+    canonical_fields = [f.lower() for f in (
+      'Location', 'Target', 'Type', 'Unit', 'Bin_start_incl',
+      'Bin_end_notincl', 'Value'
+    )]
+    field_to_column = dict(zip(canonical_fields, range(len(canonical_fields))))
+
+    # read the csv one row at a time
+    with open(filename, 'r', newline='') as f:
+      reader = csv.reader(f)
+      for row in reader:
+        # skip header row(s)
+        fields = [f.lower() for f in row]
+        if 'location' in fields:
+          # update the field-to-column-index mapping
+          field_to_column = dict(zip(fields, range(len(fields))))
+          continue
+
+        # extract values
+        values = [row[field_to_column[f]] for f in canonical_fields]
+        location, target, type_, unit, start, end, value = values
+
+        # update forecast
+        ForecastIO.__load_row(forecast, location, target, type_, start, value)
+
+    # return the group of forecasts per location
+    return forecast
+
+  @staticmethod
+  def __save_target(writer, location, target, data, unit, idx_func):
+    # convert to display names
+    location = Locations.get_display_name(location)
+    target = Targets.get_display_name(target)
+
+    # point prediction
+    type_ = Types.get_display_name('point')
+    value = ForecastIO.int_or_float(data['point'])
+    row = [location, target, type_, unit, 'NA', 'NA', value]
+    writer.writerow(row)
+
+    # distribution
+    type_ = Types.get_display_name('dist')
+    for index, value in enumerate(data['dist']):
+      start, end = idx_func(index)
+      start, end, value = map(ForecastIO.int_or_float, (start, end, value))
+      row = [location, target, type_, unit, start, end, value]
+      writer.writerow(row)
+
+    # probability of no onset (if applicable)
+    if 'none' in data:
+      value = ForecastIO.int_or_float(data['none'])
+      row = [location, target, type_, unit, 'none', 'none', value]
+      writer.writerow(row)
+
+  @staticmethod
+  def __save_week_target(writer, location, target, data):
+    def idx_func(index):
+      week = ForecastIO.get_index_week(index)
+      return week, week + 1
+    unit = 'week'
+    ForecastIO.__save_target(writer, location, target, data, unit, idx_func)
+
+  @staticmethod
+  def __save_ili_target(writer, location, target, data):
+    def idx_func(index):
+      start, end = index / 10, (index + 1) / 10
+      if index == 130:
+        end = 100
+      return start, end
+    unit = 'percent'
+    ForecastIO.__save_target(writer, location, target, data, unit, idx_func)
+
+  @staticmethod
+  def save_csv(forecast, filename=None):
+    if filename is None:
+      now = datetime.datetime.now()
+      week = forecast.epiweek % 100
+      args = (week, forecast.team, now.year, now.month, now.day)
+      filename = 'EW%02d-%s-Mturk-%04d-%02d-%02d.csv' % args
+
+    # write the csv one row at a time
+    with open(filename, 'w', newline='') as f:
+      dialect = csv.excel()
+      dialect.lineterminator = '\n'
+      writer = csv.writer(f, dialect=dialect)
+
+      # write the header row
+      writer.writerow([
+        'Location', 'Target', 'Type', 'Unit', 'Bin_start_incl',
+        'Bin_end_notincl', 'Value'
+      ])
+
+      # write each location
+      for location in forecast.get_locations():
+
+        # location-specific helper functions
+        def save_week_target(target, data):
+          ForecastIO.__save_week_target(writer, location, target, data)
+
+        def save_ili_target(target, data):
+          ForecastIO.__save_ili_target(writer, location, target, data)
+
+        # write each target
+        fc = forecast.get_forecast(location)
+        if fc.has_onset():
+          save_week_target('onset', fc.get_onset())
+        save_week_target('peakweek', fc.get_peakweek())
+        save_ili_target('peak', fc.get_peak())
+        for i in range(1, 5):
+          save_ili_target('x%d' % i, fc.get_lookahead(i))
+
+    return filename
+
+  @staticmethod
+  def import_json_delphi(json_str):
+    obj = json.loads(json_str)
+    if obj['_version'] > ForecastIO._version:
+      raise Exception('unable to import version: %d' % obj['_version'])
+    team, season, epiweek = obj['name'], obj['season'], obj['epiweek']
+    timestamp = None
+    forecast = Forecast(team, timestamp, season, epiweek)
+    data = obj['data']
+    for location in data.keys():
+      fc = forecast.get_or_create_forecast(location)
+      fc.data = data[location]
+    return forecast
+
+  @staticmethod
+  def export_json_delphi(forecast):
+    return json.dumps({
+      '_version': ForecastIO._version,
+      'name': forecast.team,
+      'season': forecast.season,
+      'epiweek': forecast.epiweek,
+      'year_weeks': forecast.season_length + 19,
+      'season_weeks': forecast.season_length,
+      'ili_bins': forecast.num_ili_bins,
+      'ili_bin_size': forecast.ili_bin_size,
+      'data': dict(
+        (location, forecast.get_forecast(location).data)
+        for location in forecast.get_locations()
+      ),
+    })
+
+  @staticmethod
+  def import_json_flusight(json_str, team, timestamp, season, epiweek):
+    raise NotImplementedError()
+
+  @staticmethod
+  def export_json_flusight(forecast):
+    raise NotImplementedError()
