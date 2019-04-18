d3e434f23410f2fbdb3f9ba6065c5fcffff69b66
ChrisSheng97 <chrissheng97@gmail.com>
2019-04-17 21:14:54 -0400

    snapshot - run epicast-mturk for 201910

--- src/main/submit.py
+++ src/main/submit.py
@@ -380,7 +380,7 @@ if __name__ == '__main__':
   submit(args.plotdir, not args.onlyaf, not args.onlyec, args.insane, epiweek, args.store, args.email, args.upload, args.ecround, args.test)
   
   # hospitalization
-  epiweek = EpiDate.today().add_weeks(-2).get_ew()
+  epiweek = EpiDate.today().add_weeks(-6).get_ew()
   ec_age_groups = ['rate_overall', 'rate_age_0', 'rate_age_1', 'rate_age_2', 'rate_age_3', 'rate_age_4']
   sub_hosp = Submissions_Hosp(ec_age_groups, 1000)
   ec_hosp = None
